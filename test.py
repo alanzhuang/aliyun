16ada0904066d7d0afbe2b58d5851dfe92bc752f
zhuangjie <zhuangjie1108@cashbus.com>
2018-04-25 10:40:51 +0800

    1

--- test.py
+++ test.py
@@ -11,49 +11,20 @@ warnings.filterwarnings("ignore")
 def runc(info, db, r, pp):
     url = info['url']
     keyword = info['keyword']
-    if 'title' not in info.keys():
-        try:
-            resp,pp = get_resp(url, r, pp)
-        except:
-            print('获取文章列表失败'+url)
-            r.sadd("tandfon_list",info)
+    title = info['title']
+    publish_time = info['publish_time']
+    try:
+        resp, pp = get_resp(url, r, pp)
+    except:
+        print('获取文章详情失败' + url)
+        r.sadd("tandfon_url", info)
 
-        else:
-            if resp:
-                tree = html.fromstring(resp)
-                results = tree.xpath('//form[@id="frmSearchResults"]/ol[@class="search-results"]/li')
-                for result in results:
-                    title = result.xpath('article/@data-title')[0]
-                    publish_time = result.xpath('article/div[@class="searchentryright"]/div[last()]/span/text()')[0]
-                    href = result.xpath('article/div[1]/span[1]/a[@class="ref nowrap"]/@href')[0]
-                    url = 'https://www.tandfonline.com' + href
-                    try:
-                        resp,pp = get_resp(url, r, pp)
-                    except:
-                        info['url'] = url
-                        info['title'] = title
-                        info['publish_time'] = publish_time
-
-                        print('获取文章详情失败' + url)
-                        r.sadd("tandfon_list",info)
-
-                    else:
-                        if resp:
-                            get_detail(resp, db, title, keyword, publish_time,url)
     else:
-        title = info['title']
-        publish_time = info['publish_time']
-        try:
-            resp, pp = get_resp(url, r, pp)
-        except:
-            print('获取文章详情失败' + url)
-            r.sadd("tandfon_list",info)
+        if resp:
+            get_detail(resp, db, title, keyword, publish_time, url)
 
-        else:
-            if resp:
-                get_detail(resp, db, title, keyword, publish_time,url)
 
-def get_detail(resp,db,title,keyword,publish_time,url):
+def get_detail(resp, db, title, keyword, publish_time, url):
     tree = html.fromstring(resp)
     authors_info = tree.xpath('//span[@class="NLM_contrib-group"]')
     if authors_info:
@@ -62,11 +33,11 @@ def get_detail(resp,db,title,keyword,publish_time,url):
         for author in authorsWithEmail:
             name = author.xpath('a[@class="entryAuthor"]/text()')[0]
             email = author.xpath('a[@class="entryAuthor"]/span/span/span[@class="corr-email"]/text()')[0]
-            raw = name+email+title+keyword+publish_time
+            raw = name + email + title + keyword + publish_time
             rawdata = hashlib.md5(raw.encode('utf-8')).hexdigest()
             cursor = db.cursor()
             sql = "INSERT INTO zhuang(title,keyword,publication_time,author,email,rawdata,url)VALUES ('%s','%s','%s', '%s', '%s', '%s', '%s')" % (
-                pymysql.escape_string(title), keyword, publish_time.strip(), name, email,rawdata,url)
+                pymysql.escape_string(title), keyword, publish_time.strip(), name, email, rawdata, url)
             print(sql)
             try:
                 cursor.execute(sql)
@@ -84,12 +55,12 @@ def get_resp(url, r, pp):
     while i < 4:
         try:
             print('使用代理：' + str(pp))
-            resp = requests.get(url, headers=headers, verify=False, timeout=20, proxies=eval(pp)).text
+            resp = requests.get(url, headers=headers, verify=False, timeout=5, proxies=eval(pp)).text
         except Exception as e:
             i += 1
             r.srem('proxy_list', pp)
-            print('发生错误'+str(e))
-            print('删除代理:'+str(pp))
+            print('发生错误' + str(e))
+            print('删除代理:' + str(pp))
             pp = get_proxy(r)
             import traceback
             print('第' + str(i) + '次部署')
@@ -97,37 +68,14 @@ def get_resp(url, r, pp):
             break
     if i == 4:
         raise Exception
-    return resp,pp
+    return resp, pp
 
 
 def get_proxy(r):
     pp = r.srandmember('proxy_list')
     while not pp:
-        resp = requests.get(
-            'http://api.xdaili.cn/xdaili-api//privateProxy/applyStaticProxy?spiderId=287e0dc52d2c4e6cb878a29044be25f6&returnType=2&count=1')
-        """
-        {
-        "ERRORCODE":"0",
-        "RESULT":[
-        {"port":"43617","ip":"222.85.5.118"},
-        {"port":"43569","ip":"180.122.20.108"},
-        {"port":"20443","ip":"221.230.254.73"}
-        ]}
-        """
-        pp_dict = resp.json()
-        if pp_dict['ERRORCODE'] == '0':
-            for info in pp_dict['RESULT']:
-                print('得到代理：' + str(info))
-                # {‘http':'117.25.189.249:28492',‘https':'117.25.189.249:28492'}
-                x = {}
-                x['http'] = info['ip'] + ':' + info['port']
-                x['https'] = info['ip'] + ':' + info['port']
-                r.sadd("proxy_list", x)
-        else:
-            print(pp_dict['ERRORCODE'])
-            time.sleep(10)
-
-        pp = r.srandmember('proxy_list')
+        time.sleep(10)
+        return get_proxy(r)
     return pp
 
 
@@ -135,22 +83,23 @@ def main():
     import redis, os
     redis_host = os.getenv('redis_host', '118.25.19.129')
     redis_port = os.getenv('redis_port', '6379')
-    redis_password = os.getenv('redis_password', '')
+    redis_password = os.getenv('redis_password', '54panjiajia')
     r = redis.Redis(
         host=redis_host,
         port=int(redis_port),
         password=redis_password,
         decode_responses=True
     )
-    mysql_password = os.getenv('mysql_password', '')
-    db = pymysql.connect(host="sh-cdb-gsw750ll.sql.tencentcdb.com", port=63599, user="root", password=mysql_password,
-                         db="TESTDB", charset='utf8')
+    db = pymysql.connect(host="sh-cdb-gsw750ll.sql.tencentcdb.com", port=63599, user="root", password="520liziying", db="TESTDB", charset='utf8')
     pp = get_proxy(r)
     while True:
-        info_dict = r.spop('tandfon_list')
+        info_dict = r.spop('tandfon_url')
         if info_dict:
             info = eval(info_dict)
-            runc(info, db, r, pp)
+            try:
+                runc(info, db, r, pp)
+            except Exception:
+                r.sadd('tandfon_url', info_dict)
         else:
             print('全部完成')
             break
